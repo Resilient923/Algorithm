676024881f68fd4232e787ee12f826d4eacdd9dc
Resilient923 <kc960923@naver.com>
2021-03-20 15:17:59 +0900

    [update] 10815 210320

---
+++ BaekJoon/10815.py
@@ -0,0 +1,49 @@
+#처음작성한 코드 시간초과
+import sys
+n = int(sys.stdin.readline())
+n_data=(list(map(int,sys.stdin.readline().split())))
+m = int(sys.stdin.readline())
+m_data=(list(map(int,sys.stdin.readline().split())))
+dp = [0 for i in range(m)]
+for i in range(len(m_data)):
+    for j in range(len(n_data)):
+        if (m_data[i]==n_data[j]):
+            dp[i]=1
+print(dp)
+#두번째코드
+n = int(sys.stdin.readline())
+n_data=set(map(int,input().split()))
+m = int(sys.stdin.readline())
+m_data=(list(map(int,sys.stdin.readline().split())))
+for i in m_data:
+    if i in n_data:
+        print(1, end=' ')
+    else:
+        print(0, end=' ')
+
+#이진탐색풀이
+import sys
+input = sys.stdin.readline
+n = int(input())
+s = list(map(int, input().split()))
+m = int(input())
+s_ = list(map(int, input().split()))
+s.sort()
+for i in s_:
+    low, high = 0, n
+    while low <= high:
+        mid = (low + high) // 2
+        if 0 <= mid < n:
+            if s[mid] < i: 
+                low = mid + 1
+            else: 
+                high = mid - 1
+        else: 
+            break
+    if 0 <= high + 1 < n:
+        if s[high + 1] == i: 
+            print(1, end=" ")
+        else: 
+            print(0, end=" ")
+    else:
+         print(0, end=" ")
