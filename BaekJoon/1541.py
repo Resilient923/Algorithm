96ad6854973c4b8ccf404b19ad2e2b2c31fff6af
Resilient923 <kc960923@naver.com>
2021-04-04 00:00:49 +0900

    [add] 210404

---
+++ BaekJoon/1541.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+data = input().split('-')
+ans = 0
+#-가 처음으로 나오고 뒤에 다 더하면된다.
+for i in data[0].split('+'):
+    ans += int(i)
+for i in data[1:]:
+    for j in i.split('+'):
+        ans -= int(j)
+print(ans)
+print(data)
---
+++ BaekJoon/2588.py
@@ -0,0 +1,8 @@
+a = int(input())
+b = input()
+
+
+print(a*int(b[2]))
+print(a*int(b[1]))
+print(a*int(b[0]))
+print(a*int(b[0])*100+a*int(b[1])*10+a*int(b[2]))
