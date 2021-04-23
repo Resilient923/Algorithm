1b21ada5a780818693ee3c156a47b3672c8015ab
Resilient923 <kc960923@naver.com>
2021-04-24 00:36:11 +0900

    [update] 210424

--- BaekJoon/1978.py
+++ BaekJoon/1978.py
@@ -1,7 +1,13 @@
 n = int(input())
 data = list(map(int,input().split()))
+""" if 1 in data:
+    del data[0]
+data = set(list(map(int,data)))
 
-cnt =0
+for i in range(2, int(max(data) ** 0.5) + 1):
+    data -= set(range(i * 2, max(data) + 1, i))
+print(len(data)) """
+""" cnt =0
 for i in data:
     if i !=1:
         if i ==2:
@@ -12,9 +18,21 @@ for i in data:
             cnt+=1
         elif(i-1)%6==0:
             cnt+=1
-    
+     """
+def prime_number(num):
+    if num == 1:
+        return False
+    elif num == 2:
+        return True
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+cnt = 0
+for i in data:
+    if prime_number(i)==True:
+        cnt +=1
 print(cnt)
-    
     
     
     
