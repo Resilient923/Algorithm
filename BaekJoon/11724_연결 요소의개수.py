a641df6a6210d8eec363fcca6ff25587b8b6cde4
Resilient923 <kc960923@naver.com>
2021-05-14 00:12:24 +0900

    [update] 210513

--- BaekJoon/11724_연결 요소의개수.py
+++ BaekJoon/11724_연결 요소의개수.py
@@ -16,7 +16,7 @@ for _ in range(m):
     node1,node2 = map(int,input().split())
     graph[node1].append(node2)
     graph[node2].append(node1)
-
+print(graph)
 cnt = 0
 for i in range(1,n+1):
     if visited[i] == False:
