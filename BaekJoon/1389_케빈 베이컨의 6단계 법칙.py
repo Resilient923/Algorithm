e583100c4feae606519eace177c47ee3ba1197ab
Resilient923 <kc960923@naver.com>
2021-06-03 01:41:34 +0900

    [add] 210602

---
+++ BaekJoon/1389_케빈 베이컨의 6단계 법칙.py
@@ -0,0 +1,82 @@
+#bfs문제
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+graph = [[] for _ in range(n+1)]
+for i in range(m):
+    a,b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+def bfs(graph,start):
+    num = [0] * (n+1)
+    q = deque()
+    visited[start] = 1
+    q.append(start)
+    while q:
+        a = q.popleft()
+        for i in graph[a]:
+            if visited[i] == 0:
+                num[i] = num[a]+1
+                q.append(i)
+                visited[i] = 1
+    return sum(num)
+
+result = []
+for i in range(1,n+1):
+    visited = [0 for _ in range(n+1)]
+    result.append(bfs(graph,i))
+print(result.index(min(result))+1)
+##########################################################플로이드워셜
+import sys
+from collections import deque
+input = sys.stdin.readline
+Min = 1000000000
+n,m = map(int,input().split())
+graph = [[0]*n for _ in range(n)]
+for _ in range(m):
+    a,b = map(int,input().split())
+    graph[a-1][b-1] = 1
+    graph[b-1][a-1] = 1
+
+for i in range(n): #거치는점
+    for j in range(n): #출발점
+        for k in range(n): #도착점
+            if j == k:
+                continue
+            elif graph[j][i] and graph[i][k]:
+                if graph[j][k] == 0:
+                    graph[j][k] = graph[j][i] + graph[i][k]
+                else:
+                    graph[j][k] = min(graph[j][k],graph[j][i] + graph[i][k])
+for i in range(n):
+    result = 0
+    for j in range(n):
+        result += graph[i][j]
+    if Min > result:
+        Min = result
+        person = i
+print(person+1)
+
+#####################################################dfs풀이(실패)
+""" def dfs(graph,start,visited,cnt):
+    cnt += 1
+    for i in graph[start]:
+        if visited[i] == 0:
+            visited[i] = 1
+            print(cnt)
+            dfs(graph,i,visited,cnt) """
+            
+
+""" answer = [[] for _ in range(n+1)]
+for i in range(1,n+1):
+    visited = [0 for _ in range(n+1)]
+    cnt = 0
+    dfs(graph,i,visited,cnt)
+    print("---")
+    answer[i].append(cnt)
+print(answer)
+print(graph) """
+
