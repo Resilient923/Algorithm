1a288aa02bfa92243ad2e6ad789f55074b89ba8a
Resilient923 <kc960923@naver.com>
2021-06-02 13:21:09 +0900

    [add] 210601

---
+++ BaekJoon/1076_저항.py
@@ -0,0 +1,6 @@
+color = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+one = color.index(input())
+two = color.index(input())
+three = color.index(input())
+result= int(str(one) + str(two)) * (10 ** three)
+print(result)
