972833c02921267c6ec5edc27b0053c696c12048
Resilient923 <kc960923@naver.com>
2021-06-06 00:00:28 +0900

    [add] 210605

---
+++ BaekJoon/6603_로또.py
@@ -0,0 +1,16 @@
+from itertools import combinations,permutations
+import sys
+input = sys.stdin.readline
+
+while True:
+    num = list(map(int,input().split()))
+    if num[0] == 0:
+        break
+    num.pop(0)
+    a = list(combinations(num,6))
+    for i in a:
+        for j in range(len(i)):
+            print(i[j],end=" ")
+        print("")
+    print("")
+
