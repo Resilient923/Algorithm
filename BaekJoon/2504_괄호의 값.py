31b6a0a1e0be5ddfb2d77d5445d282f40e97570f
Resilient923 <kc960923@naver.com>
2021-06-09 00:55:00 +0900

    [update] 주석 210608

--- BaekJoon/2504_괄호의 값.py
+++ BaekJoon/2504_괄호의 값.py
@@ -1,6 +1,7 @@
 import sys
 input = sys.stdin.readline
 
+# \n를 빼주기 위해서 restrip() 을 사용
 data = list(input().rstrip())
 
 stack = []
@@ -8,7 +9,7 @@ stack = []
 for i in data:
     if i ==')':
         flag = 0
-        while len(stack) > 0:
+        while len(stack)>0:
             compare_pop = stack.pop()
             if compare_pop == '(':
                 # 바로 옆에 ( 가 있을 때
@@ -21,13 +22,14 @@ for i in data:
             # 잘못된 괄호열
             elif compare_pop == '[':
                 print(0)
-                exit
+                # 인자값이 0이 들어가면 종료해준다.
+                exit(0)
             #숫자면
             else:
                 flag += compare_pop
     elif i ==']':
         flag = 0
-        while len(stack) > 0:
+        while len(stack)>0:
             compare_pop = stack.pop()
             if compare_pop == '[':
                 # 바로 옆에 [ 가 있을 때
@@ -40,7 +42,8 @@ for i in data:
             # 잘못된 괄호열
             elif compare_pop == '(':
                 print(0)
-                exit
+                # 인자값이 0이 들어가면 종료해준다.
+                exit(0)
             #숫자면
             else:
                 flag += compare_pop
@@ -53,6 +56,8 @@ for i in stack:
     # 아직도 괄호가 하나라도 남아 있으면 잘못된 괄호열
     if i == '(' or i == '[':
         print(0)
+        # 인자값이 0이 들어가면 종료해준다.
+        exit(0)
     else:
         result += i
 print(result)
