d1133d7800fc92f841ce3769ecc41ec789b7fdfa
Resilient923 <kc960923@naver.com>
2021-12-08 23:58:29 +0900

    211208

--- BaekJoon/14891_톱니바퀴.py
+++ BaekJoon/14891_톱니바퀴.py
@@ -1,37 +1,43 @@
 import sys
 input = sys.stdin.readline
 
-graph = [None]*4
-for i in range(4):
-    graph[i] = list(input().strip())
+graph = []
+# N이 0 이고 S가 1이다.
+for _ in range(4):
+    graph.append(list(input().rstrip()))
+k = int(input())
+data = []
 
-def dfs(x,y): # x는 바퀴번호 y는 방향  
-    #인덱스 2, 인덱스 6 맞닿는다
+def rotate_clock(graph):
+    temp = graph[7]
+    for i in range(6,-1,-1):
+        graph[i+1] = graph[i]
+    graph[0] = temp
+
+def rotate_ban_clock(graph):
+    temp = graph[0]
+    for i in range(7):
+        graph[i] = graph[i+1]
+    graph[7] = temp
+
+def dfs(x,y):
     global visited
-    if visited[x] == False:
-        visited[x] = True
+    if visited[x] == 0:
+        visited[x] = 1
         left = graph[x][6]
         right = graph[x][2]
-        if y == 1:
-            flag = graph[x][7] #flag 처리
-            for i in range(6,-1,-1):
-                graph[x][i+1] = graph[x][i]
-            graph[x][0] = flag
+        if y == 1: # 시계방향일떄
+            rotate_clock(graph[x])
         else:
-            flag  = graph[x][0]
-            for i in range(7):
-                graph[x][i] = graph[x][i+1]
-            graph[x][7] = flag
+            rotate_ban_clock(graph[x])
         if x-1 >= 0 and left != graph[x-1][2]:
-            dfs(x-1,-y)
+            dfs(x-1,-y) # 방향은 지금 톱니바퀴가 도는 방향과 반대로 돈다.
         if x+1 <=3 and right != graph[x+1][6]:
             dfs(x+1,-y)
 
-
-k = int(input())
 for _ in range(k):
     a,b = map(int,input().split())
-    visited = [False]*4
+    visited = [0] * 4
     dfs(a-1,b)
 
 cnt = 0
@@ -39,114 +45,3 @@ for i in range(4):
     if graph[i][0] =='1':
         cnt += (2**i)
 print(cnt)
-
-##########################################
-#성현풀이
-
-""" import sys
-
-def show(circle):
-    for i in range(len(circle)):
-        for s in circle[i]:
-            print(s, end = ' ')
-        print()
-
-
-def rotateRight(array):             
-    temp = array[:-1]
-    temp.insert(0,array[-1])
-    return temp
-
-def rotateLeft(array):
-    temp = array[1:]
-    temp.append(array[0])
-    return temp
-
-if __name__ == "__main__":
-    circle = []
-    for _ in range(4):
-        circle.append(list(map(int, sys.stdin.readline().rstrip())))
-    # 양 옆, 인덱스를 맞춰주기 위해 처음 마지막에 톱니바퀴 추가
-    circle.append([0,0,0,0,0,0,0,0]) 
-    circle.insert(0,[0,0,0,0,0,0,0,0])
-
-    
-    k = int(input())
-    # k번 회전
-    for _ in range(k):
-        idx, direction = map(int, input().split())
-        left, right = idx, idx
-
-        # 시계 방향 회전
-        if direction == 1:          
-            # 자기 자신 회전
-            circle[idx] = rotateRight(circle[idx])
-            
-            # 반대 극이면 반시계 방향 회전
-            rotate = True
-          # 1 = 시계방향
-            while rotate == True and left > 0:
-                rotate = False
-                if circle[left-1][2] != circle[left][6]:    
-                    if direction == 1:
-                        circle[left-1] = rotateLeft(circle[left-1]) # 반시계
-                        direction = -1
-                    elif direction == -1:
-                        circle[left-1] = rotateRight(circle[left-1]) # 시계 방향
-                        direction = 1
-                    rotate = True
-                    left -= 1
-
-            rotate = True
-            direction = 1
-            while rotate == True and right < 5:
-                rotate = False
-                if circle[right+1][6] != circle[right][2]:
-                    if direction == 1:
-                        circle[right+1] = rotateLeft(circle[right+1])
-                        direction = -1
-                    elif direction == -1:
-                        circle[right+1] = rotateRight(circle[right+1])
-                        direction = 1
-                    rotate = True
-                    right += 1
-    
-
-        # 반시계 방향 회전
-        elif direction == -1:
-            # 자기 자신 회전
-            circle[idx] = rotateLeft(circle[idx])
-            # 반대 극이면 반시계 방향 회전
-            rotate = True
-           
-            while rotate == True and left > 0:
-                rotate = False
-                if circle[left-1][2] != circle[left][6]:
-                    if direction == -1:
-                        circle[left-1] = rotateRight(circle[left-1])
-                        direction = 1
-                    elif direction == 1:
-                        circle[left-1] = rotateLeft(circle[left-1])
-                        direction = -1
-                    rotate = True
-                    left -= 1
-
-            rotate = True
-            while rotate == True and right < 5:
-                rotate = False
-                if circle[right+1][6] != circle[right][2]:
-                    if direction == -1:
-                        circle[right+1] = rotateRight(circle[right+1])
-                        direction = 1
-                    elif direction == 1:
-                        circle[right+1] = rotateLeft(circle[right+1])
-                        direction = -1
-                    rotate = True
-                    right += 1
-                    
-            
-    answer = 0
-    for i in range(4):
-        if circle[i][0] == 1:
-            answer += (2**(i-1))
-    print(answer) """
