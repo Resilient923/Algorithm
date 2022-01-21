8a03a13037eb7d6771f311a1c1e1eb4c42f1f833
shinkyuchul <kc960923@naver.com>
2022-01-21 23:41:01 +0900

    220121

--- BaekJoon/2589_보물섬.py
+++ BaekJoon/2589_보물섬.py
@@ -1,36 +1,47 @@
 import sys
 from collections import deque
 input = sys.stdin.readline
-sys.setrecursionlimit(2500)
+
 n,m = map(int,input().split())
-graph = [list(input().rstrip()) for _ in range(n)]
+graph = []
+for _ in range(n):
+    graph.append(list(input().rstrip()))
 
-dx = [1,-1,0,0]
-dy = [0,0,1,-1]
 
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+
+# BFS 함수 
 def bfs(x,y,cnt):
+    # 한 지점에서 bfs를 돌 때 현재 cnt값이 최대인지 아닌지를 구분하기 위해 
+    # max_를 갱신해준다.
+    max_ = -1
     q = deque()
     q.append((x,y,cnt))
-    visited[x][y] = 1 #방문처리
+    visited[x][y] = 1
     while q:
-        x,y,cnt= q.popleft()
+        x,y,cur_cnt = q.popleft()
+        # max_를 갱신해준다.
+        if max_ < cur_cnt:
+            max_ = cur_cnt
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0<= nx < n and 0<= ny < m and visited[nx][ny]==0 and graph[nx][ny]=='L':
+            if 0<= nx < n and 0<=ny<m and graph[nx][ny] == 'L' and visited[nx][ny] == 0:
+                # 여기서 현재 좌표의 cnt에서 다음 좌표로 넘어갈 때 cnt + 1 을 해준다.
+                q.append((nx,ny,cur_cnt+1))
                 visited[nx][ny] = 1
-                # 한칸이동할떄마다 값을 갱신해주기위해서 cnt+1을 해준다.
-                q.append((nx,ny,cnt+1))
-    return cnt
-               
+    # 갱신해줬던 max_ 값을 return 해준다/
+    return max_
 
-ans = 0
+# Max변수 선언
+Max = -1
 for i in range(n):
     for j in range(m):
+        # L 일 때만 bfs함수를 실행시켜줘야 한다.  이부분을 놓쳐서 허무하게 시간을 날렸다..
         if graph[i][j] == 'L':
-            # bfs는 최단거리로 움직이기 때문
-            # 각각 i,j 마다 bfs를 실행시켜준다.
-            visited = [[0]*m for _ in range(n)]
-            ans = max(ans,bfs(i,j,0))
+            visited = [[0] * (m) for _ in range(n)]
+            Max = max(Max,bfs(i,j,0))
+print(Max)
 
-print(ans)
+
